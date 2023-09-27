1c8fb4ffe13c463c3d5ef2ee5be9d2db4b7374e8
Atte Kivimaki <aj.kivimaki@gmail.com>
2023-09-27 19:28:42 +0300

    fix gallery

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,40 +1,43 @@
-const backToTopBtn = document.querySelector('#backToTop');
-const hamburger = document.querySelector('.mobile');
+const backToTopBtn = document.querySelector("#backToTop");
+const hamburger = document.querySelector(".mobile");
 
-const header = document.querySelector('header');
-const menuItems = document.querySelectorAll('nav ul li a');
-const nav = document.querySelector('nav ul');
+const header = document.querySelector("header");
+const menuItems = document.querySelectorAll("nav ul li a");
+const nav = document.querySelector("nav ul");
 
 const getToTop = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
 const mobMenu = () => {
-    /* closes the menu when clicked */
-    for (const item of menuItems) {
-        item.addEventListener('click', mobMenu);
-    }
-    // toggle showing mobile menu when clicking hamburger icon
-    nav.classList.toggle('responsive');
-}
+  /* closes the menu when clicked */
+  for (const item of menuItems) {
+    item.addEventListener("click", mobMenu);
+  }
+  // toggle showing mobile menu when clicking hamburger icon
+  nav.classList.toggle("responsive");
+};
 
 const scrollFunc = (e) => {
-    // add background color to header after scrolling
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        header.classList.add('bg');
-    } else {
-        header.classList.remove('bg');
-    }
+  // add background color to header after scrolling
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    header.classList.add("on-scroll");
+  } else {
+    header.classList.remove("on-scroll");
+  }
 
-    // show back to top button after a short scroll
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        backToTopBtn.style.display = "block";
-    } else {
-        backToTopBtn.style.display = "none";
-    }
-}
+  // show back to top button after a short scroll
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
 
-backToTopBtn.addEventListener('click', getToTop);
-hamburger.addEventListener('click', mobMenu);
-window.addEventListener('scroll', scrollFunc);
+backToTopBtn.addEventListener("click", getToTop);
+hamburger.addEventListener("click", mobMenu);
+window.addEventListener("scroll", scrollFunc);
